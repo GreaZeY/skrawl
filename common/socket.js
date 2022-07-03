6dbfbc16942ff5d619171324fe0740562f2a877e
GreaZeY <greaserevive@gmail.com>
2022-07-04 01:17:53 +0530

    fetch for socket endpoint

--- common/socket.js
+++ common/socket.js
@@ -1,11 +1,12 @@
 import io from 'socket.io-client'
 let socket;
-
+console.log(process.env.NODE_ENV)
 const production = 'https://skrawl-grzy.herokuapp.com';
 const development = 'http://localhost:3000';
-const url = (process.env.NODE_ENV ? production : development);
+const url = (process.env.NODE_ENV==='production' ? production : development);
 
-socket = io(url, { path: "/socket.io" });
+fetch(`${url}/api/socket`)
+socket = io();
 
 
 export default socket
