75345dbba98175a64fdef8a1a17719cbce412071
greazey <greaserevive@gmail.com>
2023-07-07 00:16:05 +0530

    dynamic base url

--- common/socket.js
+++ common/socket.js
@@ -1,6 +1,10 @@
 import io from 'socket.io-client'
-
-const url = process.env.NEXT_APP_BASE_URL || window.location.origin
+if (typeof window !== "undefined") {
+    // Client-side-only code
+}
+const localURL = typeof window === "undefined" ? 'http://localhost:3000': window.location.origin
+console.log(localURL)
+const url = process.env.NEXT_APP_BASE_URL || localURL
 
 fetch(`${url}/api/socket`)
 let socket = io();
